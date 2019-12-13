e3aa615c5d1c4496edae3aa6e9eacb59234ea694
chenmeijie <250668856@qq.com>
2019-12-13 18:02:52 +0800

    geng

---
+++ src/mainapp.java
@@ -0,0 +1,100 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class mainapp
+{
+	public static void main(String[] args)
+	{
+		String pwd = md5("123456");
+		System.out.println(pwd);
+		List<Object> list = new ArrayList<>();
+		System.out.println(list.size());
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd EEE");
+
+		Date currentDate = new Date();
+
+		// 比如今天是2012-12-25
+		List<Date> days = dateToWeek(currentDate);
+		for (Date date : days) {
+			System.out.println(sdf.format(date));
+		}
+		List<Date> days2 = dateToWeek(getLastWeekMonday(currentDate));
+		for (Date date : days2) {
+			System.out.println(sdf.format(date));
+		}
+		List<Date> days3 = dateToWeek(getNextWeekMonday(currentDate));
+		for (Date date : days3) {
+			System.out.println(sdf.format(date));
+		}
+
+
+	}
+
+
+	public static Date getLastWeekMonday(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(getThisWeekMonday(date));
+		cal.add(Calendar.DATE, -7);
+		return cal.getTime();
+	}
+
+	public static Date getThisWeekMonday(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		// 获得当前日期是一个星期的第几天
+		int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+		if (1 == dayWeek) {
+			cal.add(Calendar.DAY_OF_MONTH, -1);
+		}
+		// 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+		cal.setFirstDayOfWeek(Calendar.MONDAY);
+		// 获得当前日期是一个星期的第几天
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		// 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+		cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+		return cal.getTime();
+	}
+
+	public static Date getNextWeekMonday(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(getThisWeekMonday(date));
+		cal.add(Calendar.DATE, 7);
+		return cal.getTime();
+	}
+
+
+	public static List<Date> dateToWeek(Date mdate) {
+		int b = mdate.getDay();
+		Date fdate;
+		List<Date> list = new ArrayList<Date>();
+		Long fTime = mdate.getTime() - b * 24 * 3600000;
+		for (int a = 1; a <= 7; a++) {
+			fdate = new Date();
+			fdate.setTime(fTime + (a * 24 * 3600000));
+			list.add(a-1, fdate);
+		}
+		return list;
+	}
+
+	public static String md5(String plainText) {
+		byte[] secretBytes = null;
+		try {
+			secretBytes = MessageDigest.getInstance("md5").digest(
+					plainText.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("没有md5这个算法！");
+		}
+		String md5code = new BigInteger(1, secretBytes).toString(16);// 16进制数字
+		// 如果生成数字未满32位，需要前面补0
+		for (int i = 0; i < 32 - md5code.length(); i++) {
+			md5code = "0" + md5code;
+		}
+		return md5code;
+	}
+}
